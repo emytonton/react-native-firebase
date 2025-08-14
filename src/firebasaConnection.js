aaa1e227e5d0a294312cf517f5013a055a42bc08
Emilly Paiva <emillypaiva3260@gmail.com>
2025-08-13 21:05:14 -0300

    add config init firebase

---
+++ src/firebasaConnection.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBOv5CKkGglLPbQl9Qa9dWQLu44tqt5nII",
+  authDomain: "primeiroprojeto-a9d4e.firebaseapp.com",
+  projectId: "primeiroprojeto-a9d4e",
+  storageBucket: "primeiroprojeto-a9d4e.firebasestorage.app",
+  messagingSenderId: "624350699264",
+  appId: "1:624350699264:web:e7d97e83eb89787cce1de5"
+};
+
+
+const app = initializeApp(firebaseConfig);
